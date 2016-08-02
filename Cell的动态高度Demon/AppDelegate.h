952840a746dd46b465343327ed6501174c709979
daoxiu <daoxiu@daoxiudemac-mini.local>
2016-08-02 14:35:37 +0800

    Initial Commit

---
+++ Cell的动态高度Demon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cell的动态高度Demon
+//
+//  Created by daoxiu on 16/8/2.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cell的动态高度Demon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cell的动态高度Demon
+//
+//  Created by daoxiu on 16/8/2.
+//  Copyright © 2016年 aowokeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
